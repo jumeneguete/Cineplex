fa4c75ae4eeafa24ffc6dd1054b9882cc0150e2e
Juliana Meneguete <jumeneguete@gmail.com>
2021-05-12 13:56:16 -0300

    Rota '/session'

--- src/components/ChooseDate.js
+++ src/components/ChooseDate.js
@@ -4,15 +4,16 @@ import { useState, useEffect } from "react";
 
 export default function ChooseDate() {
     const [timetable, setTimetable] = useState([]);
-    const {idMovie} = useParams();
+    const { idMovie } = useParams();
 
     useEffect(() => {
         const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/movies/${idMovie}/showtimes`);
-        console.log(promise)
         promise.then((response) => {
             setTimetable(response.data)
         });
     }, []);
+
+    console.log(timetable.days)
 
     return (
         <main className="main-schedule">
@@ -20,16 +21,19 @@ export default function ChooseDate() {
                 <h1>Selecione o horário</h1>
 
                 <div className="movie-date-options">
-        
-                    <div className="movie-day-time">
-                        <h2>Quinta-feira - 24/06/2021</h2>
-                        <div className="time-options">
-                            <button className="time-box">13:00</button>
-                            <button className="time-box">14:50</button>
-                            <button className="time-box">15:00</button>
-                            <button className="time-box">19:00</button>
-                        </div>
-                    </div>
+                    {timetable.days === undefined ? "" :
+                        timetable.days.map(x => (
+                            <div className="movie-day-time">
+                                <h2>{x.weekday} - {x.date}</h2>
+                                <div className="time-options">
+                                    {x.showtimes.map(y =>
+                                        <Link to={`/seats/${y.id}`} >
+                                            <button className="time-box">{y.name}</button>
+                                        </Link>
+                                    )}
+                                </div>
+                            </div>
+                        ))}
                 </div>
             </div>
             <div className="confirm-movie">
